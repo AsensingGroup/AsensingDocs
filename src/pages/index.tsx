afcba150b7421e818ca93fd4b570dd5af8b562e8
luhuadong <luhuadong@163.com>
2024-05-25 16:19:16 +0800

    [docs] rename intro -> start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/intro">
+            to="/start">
             开始阅读
           </Link>
         </div>
