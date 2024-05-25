138186bfe26af57c2d9072a4d2ec75bbf7edaa0f
luhuadong <luhuadong@163.com>
2024-05-25 12:35:07 +0800

    fix editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,18 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AsensingGroup/AsensingDocs/tree/main',
+          showLastUpdateAuthor: true,
+          // showLastUpdateTime: true,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/AsensingGroup/AsensingDocs/tree/main',
+          showLastUpdateAuthor: true,
+          // showLastUpdateTime: true,
         },
         theme: {
           customCss: './src/css/custom.css',
