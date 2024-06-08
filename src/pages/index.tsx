267cd255dfabe9600e1fe85c9e5a834e66a1fd57
luhuadong <luhuadong@163.com>
2024-06-08 16:37:45 +0800

    chore: Maintenance suspended

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ function HomepageHeader() {
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <p className="hero__subtitle">{siteConfig.tagline + "（暂停维护）"}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
